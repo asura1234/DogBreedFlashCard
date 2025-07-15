a5e543a89e0026b0457c7701d0ff770c49c3721a
Dylan Liu <yliu8949@gmail.com>
2025-07-15 22:28:05 +0800

    add more test cases

--- DogBreedFlashCard/Game/Tests/FakeDogAPIService.swift
+++ DogBreedFlashCard/Game/Tests/FakeDogAPIService.swift
@@ -22,6 +22,26 @@ class FakeDogAPIService: DogAPIServiceProtocol {
         .init(
             imageURL: "https://images.dog.ceo/breeds/poodle/n02113799_1001.jpg",
             breed: Breed(mainBreed: "poodle", subBreed: nil)
+        ),
+        .init(
+            imageURL: "https://images.dog.ceo/breeds/dalmatian/cooper1.jpg",
+            breed: Breed(mainBreed: "dalmatian", subBreed: nil)
+        ),
+        .init(
+            imageURL: "https://images.dog.ceo/breeds/setter-gordon/n02101006_3062.jpg",
+            breed: Breed(mainBreed: "gordon", subBreed: "setter")
+        ),
+        .init(
+            imageURL: "https://images.dog.ceo/breeds/deerhound-scottish/n02092002_5152.jpg",
+            breed: Breed(mainBreed: "deerhound", subBreed: "scottish")
+        ),
+        .init(
+            imageURL: "https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_815.jpg",
+            breed: Breed(mainBreed: "elkhound", subBreed: "norwegian")
+        ),
+        .init(
+            imageURL: "https://images.dog.ceo/breeds/sheepdog-indian/Himalayan_Sheepdog.jpg",
+            breed: Breed(mainBreed: "Sheepdog", subBreed: "Himalayan")
         )
     ]
     
@@ -30,7 +50,12 @@ class FakeDogAPIService: DogAPIServiceProtocol {
         .init(mainBreed: "bulldog", subBreeds: ["boston"]),
         .init(mainBreed: "retriever", subBreeds: ["golden"]),
         .init(mainBreed: "labrador", subBreeds: []),
-        .init(mainBreed: "poodle", subBreeds: [])
+        .init(mainBreed: "poodle", subBreeds: []),
+        .init(mainBreed: "dalmatian", subBreeds: []),
+        .init(mainBreed: "gordon", subBreeds: ["setter"]),
+        .init(mainBreed: "deerhound", subBreeds: ["scottish"]),
+        .init(mainBreed: "elkhound", subBreeds: ["norwegian"]),
+        .init(mainBreed: "Sheepdog", subBreeds: ["Himalayan"])
     ]
     
     func fetchRandomDogImage() async throws -> DogImage {
