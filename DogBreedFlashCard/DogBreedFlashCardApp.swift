98fef172502389bc28252c9e6d9c57d390913843
Dylan Liu <yliu8949@gmail.com>
2025-07-15 12:24:17 +0800

    Initial Commit

---
+++ DogBreedFlashCard/DogBreedFlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedFlashCardApp.swift
+//  DogBreedFlashCard
+//
+//  Created by Dylan Liu on 2025/7/15.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedFlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DogBreedFlashCardTests/DogBreedFlashCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedFlashCardTests.swift
+//  DogBreedFlashCardTests
+//
+//  Created by Dylan Liu on 2025/7/15.
+//
+
+import Testing
+@testable import DogBreedFlashCard
+
+struct DogBreedFlashCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
